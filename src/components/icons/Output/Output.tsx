eeb27029d02e3862bc5097bdaae1defa25133b69
Hakeem Abbas <hakeemsyd@gmail.com>
2025-08-13 04:17:30 +0500

    fixed issues

--- src/components/icons/Output/Output.tsx
+++ src/components/icons/Output/Output.tsx
@@ -12,10 +12,10 @@ const Output = () => {
       <path
         d="M5 16.6667L9.16667 12.5L5 8.33334M5 31.6667L9.16667 27.5L5 23.3333M16.6667 10H35M16.6667 20H35M16.6667 30H35"
         stroke="#E9DBBD"
-        stroke-opacity="0.9"
-        stroke-width="2.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeOpacity="0.9"
+        strokeWidth="2.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );

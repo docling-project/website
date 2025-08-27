95193fab932ed221c5d7397bf05009abefc77c14
Hakeem Abbas <hakeemsyd@gmail.com>
2025-08-27 23:17:41 +0500

    update landing page
    Signed-off-by: Hakeem Abbas <hakeemsyd@gmail.com>

--- src/components/pages/LandingPage/Hero/Hero.tsx
+++ src/components/pages/LandingPage/Hero/Hero.tsx
@@ -1,10 +1,12 @@
+"use client";
 //Components
 import Display from "@/components/ui/Display";
 
 //Styles
 import styles from "./styles.module.scss";
 import Text from "@/components/ui/text";
-import Form from "../Form";
+import StaticImage from "@/components/ui/StaticImage";
+import Button from "@/components/ui/Button";
 
 const Hero = () => {
   return (
@@ -16,7 +18,7 @@ const Hero = () => {
           </Text>
           <Display size={700} className={styles.hero_title}>
             {
-              "Do you want to learn how to build agentic apps and AI pipelines that leverage your complex PDFs, Word docs, and tables? "
+              "Learn how to build agentic apps and AI pipelines that leverage your complex PDFs, Word docs, and tables "
             }
           </Display>
           <Text
@@ -26,8 +28,20 @@ const Hero = () => {
           >
             {"Join Us Thursday, September 11, 9:00 AM PT  "}
           </Text>
+          <Button
+            text={"Add to Calendar"}
+            className={`${styles.dark_button} ${styles.mob_button}`}
+            onClick={() => {}}
+          />
         </div>
-        <Form className={styles.hero_form} />
+        <StaticImage
+          src={"/images/assistant.webp"}
+          alt={"live image"}
+          width={612}
+          height={412}
+          priority
+          className={styles.hero_image}
+        />
       </div>
     </section>
   );

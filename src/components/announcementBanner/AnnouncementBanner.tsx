3ca9d1e87c3e25ede1d7caff2b83d5284abbe5af
Alex Leventer <3254549+alexleventer@users.noreply.github.com>
2025-12-02 09:48:21 -0800

    Fix space

--- src/components/announcementBanner/AnnouncementBanner.tsx
+++ src/components/announcementBanner/AnnouncementBanner.tsx
@@ -10,8 +10,8 @@ const AnnouncmentBanner = () => {
       <div className="d-flex justify-content-center w-100">
         <div className={styles.banner}>
           <Text weight={Weight.Medium} size={300} tagName="p">
-            First Docling Office Hours on Wednesday, December 11th at 10:00 AM{" "}
-            ET.
+            First Docling Office Hours on Wednesday, December 11th at 10:00 AM
+            ET.{" "}
             <a
               href="https://zoom-lfx.platform.linuxfoundation.org/meeting/95733266721?password=c4dde8b6-8371-4139-a1d4-44c20df28005"
               target="_blank"
